16f3af124093a38ec57d7d275e7186ff4098a772
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2023-11-12 01:46:09 +0530

    Changed path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+const path = require("path")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
-  basePath: ""
+  basePath: "",
+  // webpack: (config,
+  //   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@/*': path.resolve(__dirname, 'src/*'),
+  //   }
+  //   return config
+  // },
 };
 
 module.exports = nextConfig;
