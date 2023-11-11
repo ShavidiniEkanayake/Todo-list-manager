9154f08697d7d7ee38985ac7cee1a55bffc82a64
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2023-11-11 16:41:18 +0530

    Removed unwanted data

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import TodoList from '../components/TodoList';
-import { addTodo } from '../redux/actions/todoAction';
 import Header from '../components/Header'
 
 const Home = () => {
