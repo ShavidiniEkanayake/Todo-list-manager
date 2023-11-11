c5838c2769360de78cb9d91995d11b127cee69ae
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2023-11-11 12:37:30 +0530

    changed in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,19 @@
 import React, { useState } from 'react';
 import logoFull from '../assets/logos/LogoFull - light.svg'
 import Image from 'next/image';
-import { useDispatch } from 'react-redux';
 import MenuIcon from '@mui/icons-material/Menu';
 import ProfilePic from '../assets/Images/profile.png'
-import { Breadcrumb } from 'antd';
 
 const Header = () => {
-
     return (
-        <div className='px-6'>
+        <div className='px-8'>
             <div className="bg-white pl-6 shadow-sm max-w-full">
-                <div className="flex justify-between items-center">
-                    <MenuIcon></MenuIcon>
-                    <div>
-                        <Breadcrumb >
-                            <Breadcrumb.Item className="text-md font-TTHovesProTrialDemiBold text-darkblue">
-                                Home
-                            </Breadcrumb.Item>
-                            <Breadcrumb.Item className="text-md font-TTHovesProTrialDemiBold text-lightblue">
-                                Todo
-                            </Breadcrumb.Item>
-                        </Breadcrumb>
+                <div className="flex items-center">
+                    <div className="flex items-center">
+                        <MenuIcon></MenuIcon>
+                        <span className="ml-2 font-TTHovesProTrialDemiBold text-lg">My Todos</span>
                     </div>
-                    <div className="flex items-center mr-6 py-2">
+                    <div className="flex items-center ml-auto mr-6 py-2">
                         <Image
                             src={ProfilePic}
                             alt="Profile"
@@ -33,14 +23,6 @@ const Header = () => {
                             className="ml-1 flex flex-row"
                             onClick={() => navigateToLogin()}
                         >
-                            <div className="flex flex-col pr-2">
-                                <span className="text-darkblue font-TTHovesProTrialDemiBold ml-2 text-sm">
-                                    Loro Freezai
-                                </span>
-                                <span className="text-[#b6bcc1] font-TTHovesProTrialDemiBold ml-2 text-xs uppercase">
-                                    Student
-                                </span>
-                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -12,50 +12,7 @@ const Home = () => {
 
   return (
     <div>
-      <Header />
-      <div className='px-5'>
-        <nav className="bg-white mt-[0.10rem]">
-        <div className="w-full pl-6 py-3 pr-6 shadow-sm flex items-center justify-between">
-          <ul className="flex flex-row font-medium space-x-8">
-            <li>
-              <a
-                href="#"
-                className="text-darkblue hover:text-lightblue text-sm font-TTHovesProTrialMedium "
-                aria-current="page"
-              >
-                <AddRoundedIcon className="text-lightblue mr-1" /> Create New
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-darkblue hover:text-lightblue text-sm font-TTHovesProTrialMedium"
-              >
-                <RefreshRoundedIcon className="text-lightblue mr-1 w-5" />{" "}
-                Refresh
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-darkblue hover:text-lightblue text-sm font-TTHovesProTrialMedium"
-              >
-                <SettingsRoundedIcon className="text-lightblue mr-1 w-5" /> Manage View
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="text-darkblue hover:text-lightblue text-sm font-TTHovesProTrialMedium"
-              >
-                <FileDownloadOutlinedIcon className="text-lightblue mr-1 w-5" /> Export
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      </div>
-      
+      <Header />    
       <TodoList />
     </div>
   );
