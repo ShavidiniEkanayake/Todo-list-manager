5398a00c8419083a98af72d07c3cd7d35706b49f
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2023-11-14 00:41:58 +0530

    Fixed responsive way of loging page

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="w-1/3 p-10 mx-auto border border-[#ababab] rounded-lg">
+      <div className="sm:w-1/2 md:w-1/2 lg:w-1/3 p-10 mx-auto border border-[#ababab] rounded-lg">
         <div className="text-center">
           <p className="font-TTHovesProTrialDemiBold text-3xl pb-3">My Todos</p>
           <h2 className="font-TTHovesProTrialDemiBold text-darkblue text-sm text-center">
